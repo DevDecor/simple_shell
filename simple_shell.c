ad7597a5b7229ca9aee3dc082e599459d2b419e4
Coder <fiyiziyech@gmail.com>
2022-11-09 12:00:27 +0100

    Initial commit

---
+++ simple_shell.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - The main entry
+ * @ac: argument counter
+ * @av: argument vector
+ * Return: nothing
+ */
+int main(int ac, char **av)
+{
+	printf("Hello World!");
+	return (0);
+}
